a7a924e41bf7aa21a67ff65ade4d99adf157160b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-26 23:30:06 -0500

    Add comments

--- key-counter/app.js
+++ key-counter/app.js
@@ -1,20 +1,22 @@
-let counter = document.getElementById("count");
-let counterValue = document.getElementById("counter");
-let updateCount = document.getElementById("updateCount");
-let count = 0;
+let counter = document.getElementById("count"); //key count since `Add # to counter` button is pressed
+let counterValue = document.getElementById("counter"); //counter value
+let updateCount = document.getElementById("updateCount"); //
+let count = 0; //set key count to 0 when page is loaded
 
 document.addEventListener("keypress", () => {
+    //increase key count when key is pressed
     count++;
     counterValue.value = count;
-    updateCount.value = `Add ${count} to counter`;
+    updateCount.value = `Add ${count} to counter`; //update button text
 });
 updateCount.addEventListener("click", async () => {
+    //when `Add # to counter` button is pressed
     const request = await fetch("/increase-counter", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: `counter=${count}`,
     }).then((response) => response.json());
-    count = 0;
+    count = 0; //set key counter to 0
     counterValue.value = 0;
     counter.textContent = request;
     updateCount.value = "Add 0 to counter";
--- key-counter/key-counter.go
+++ key-counter/key-counter.go
@@ -8,21 +8,24 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./"))
-	http.Handle("/", fs)
-	var count int = 0
+	fs := http.FileServer(http.Dir("./")) //use root directory ("/") for file server
+	http.Handle("/", fs)                  //handle HTML requests to "/" (the root page)
+	var count int = 0                     //initialize key count to 0
+	//Sends a POST request
+	//w - HTTP response handler
+	//r - HTTP request
 	http.HandleFunc("/increase-counter", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			var counter int
-			counter, err := strconv.Atoi(r.FormValue("counter"))
-			count += counter
-			log.Printf("Counter: %d", count)
-			fmt.Fprintf(w, "%d", count)
+		if r.Method == http.MethodPost { //send POST request
+			var counter int                                      //initialize counter variable to increase key count
+			counter, err := strconv.Atoi(r.FormValue("counter")) //parse counter form variable to int
+			count += counter                                     //increase count by counter
+			log.Printf("Counter: %d", count)                     //log the total key count
+			fmt.Fprintf(w, "%d", count)                          //write POST response data
 			if err != nil {
-				panic(err)
+				panic(err) //stop if there is an error (error due to not a number when parsing to int)
 			}
 		}
 	})
-	log.Print("Serving at localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	log.Print("Serving at localhost:8080") //log when server starts
+	http.ListenAndServe(":8080", nil)      //serve the web application at localhost:8080
 }
