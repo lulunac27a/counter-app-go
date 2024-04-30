85322b2bb8f182d036aafdcdb41a6a5481c9ec4e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-29 21:15:44 -0500

    Add comments

--- key-counter-cli/key-counter-cli-unix.go
+++ key-counter-cli/key-counter-cli-unix.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	cmd := exec.Command("stty", "raw", "-echo")
-	cmd.Stdin = os.Stdin
-	err := cmd.Run()
+	cmd.Stdin = os.Stdin //standard input
+	err := cmd.Run()     //run the command
 	if err != nil {
 		panic(err)
 	}
@@ -22,23 +22,23 @@ func main() {
 			panic(errs)
 		}
 	}()
-	var counter int = 0
-	var count int = 0
-	key := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter any key:")
+	var counter int = 0              //set counter to 0
+	var count int = 0                //set key count to 0
+	key := bufio.NewReader(os.Stdin) //read standard input
+	fmt.Print("Enter any key:")      //print the string
 	for {
-		text, _, errors := key.ReadRune()
+		text, _, errors := key.ReadRune() //read rune input
 		if errors != nil {
 			panic(errors)
 		}
-		if text == '\r' {
-			counter += count
-			count = 0
+		if text == '\r' { //when enter key is pressed
+			counter += count //increase counter by number of keys pressed
+			count = 0        //set count to 0
 			fmt.Printf("\nCounter: %d", counter)
-		} else if text == 27 {
+		} else if text == 27 { //when ESC key is pressed
 			break
 		} else {
-			count++
+			count++ //increase key count when key is pressed
 		}
 	}
 }
